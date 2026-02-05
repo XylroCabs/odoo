60339439977562f9014624be9a3f343574e41aa0
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 13:24:20 +0300

    Update profit_loss_wizard.py

--- addons/smarternows_accounts_app/models/profit_loss_wizard.py
+++ addons/smarternows_accounts_app/models/profit_loss_wizard.py
@@ -7,14 +7,21 @@ class ProfitLossWizard(models.TransientModel):
     date_from = fields.Date(string="Start Date", required=True)
     date_to = fields.Date(string="End Date", required=True)
 
-    def action_generate_profit_loss(self):
-        return {
-            'type': 'ir.actions.report',
-            'report_name': 'smarternows_accounts_app.profit_loss_template',
-            'report_type': 'qweb-pdf',
-            'context': {
-                'date_from': self.date_from,
-                'date_to': self.date_to,
-            }
-        }
+    def get_profit_loss_lines(self):
+        domain = [
+            ('date', '>=', self.date_from),
+            ('date', '<=', self.date_to),
+            ('move_id.state', '=', 'posted'),
+            ('account_id.user_type_id.type', 'in', ['income', 'expense']),
+        ]
+        return self.env['account.move.line'].read_group(
+            domain,
+            ['debit:sum', 'credit:sum', 'balance:sum'],
+            ['account_id']
+        )
 
+    def action_generate_profit_loss(self):
+        data = {'lines': self.get_profit_loss_lines(),
+                'date_from': self.date_from,
+                'date_to': self.date_to}
+        return self.env.ref('smarternows_accounts_app.action_report_profit_loss').report_action(None, data=data)
