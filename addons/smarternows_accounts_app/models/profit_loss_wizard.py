c2ac4f17fb1fe49c70dfdebc68b5b975b19ed438
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 12:50:04 +0300

    Update profit_loss_wizard.py

--- addons/smarternows_accounts_app/models/profit_loss_wizard.py
+++ addons/smarternows_accounts_app/models/profit_loss_wizard.py
@@ -4,5 +4,17 @@ class ProfitLossWizard(models.TransientModel):
     _name = 'profit.loss.wizard'
     _description = 'Profit & Loss Wizard'
 
-    date_from = fields.Date(string="Start Date")
-    date_to = fields.Date(string="End Date")
+    date_from = fields.Date(string="Start Date", required=True)
+    date_to = fields.Date(string="End Date", required=True)
+
+    def action_generate_profit_loss(self):
+        return {
+            'type': 'ir.actions.report',
+            'report_name': 'smarternows_accounts_app.profit_loss_template',
+            'report_type': 'qweb-pdf',
+            'context': {
+                'date_from': self.date_from,
+                'date_to': self.date_to,
+            }
+        }
+
