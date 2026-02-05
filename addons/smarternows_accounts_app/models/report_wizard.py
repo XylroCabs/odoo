6a44d4ea609ed4ff22c41b81bbf6468b81604b5c
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 12:39:15 +0300

    Create report_wizard.py

---
+++ addons/smarternows_accounts_app/models/report_wizard.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class ReportWizard(models.TransientModel):
+    _name = 'report.wizard'
+    _description = 'Report Wizard'
+
+    date_from = fields.Date(string="Start Date")
+    date_to = fields.Date(string="End Date")
