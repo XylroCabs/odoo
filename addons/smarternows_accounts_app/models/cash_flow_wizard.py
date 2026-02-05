d041da3e90a80f3dae2d81f40090893776b40054
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 13:25:49 +0300

    Update cash_flow_wizard.py

--- addons/smarternows_accounts_app/models/cash_flow_wizard.py
+++ addons/smarternows_accounts_app/models/cash_flow_wizard.py
@@ -7,13 +7,21 @@ class CashFlowWizard(models.TransientModel):
     date_from = fields.Date(string="Start Date", required=True)
     date_to = fields.Date(string="End Date", required=True)
 
+    def get_cash_flow_lines(self):
+        domain = [
+            ('date', '>=', self.date_from),
+            ('date', '<=', self.date_to),
+            ('move_id.state', '=', 'posted'),
+            ('account_id.user_type_id.type', '=', 'liquidity'),
+        ]
+        return self.env['account.move.line'].read_group(
+            domain,
+            ['debit:sum', 'credit:sum', 'balance:sum'],
+            ['account_id']
+        )
+
     def action_generate_cash_flow(self):
-        return {
-            'type': 'ir.actions.report',
-            'report_name': 'smarternows_accounts_app.cash_flow_template',
-            'report_type': 'qweb-pdf',
-            'context': {
+        data = {'lines': self.get_cash_flow_lines(),
                 'date_from': self.date_from,
-                'date_to': self.date_to,
-            }
-        }
+                'date_to': self.date_to}
+        return self.env.ref('smarternows_accounts_app.action_report_cash_flow').report_action(None, data=data)
