5f140e2a351a8b6ca612b70d927f939b542bd83c
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 12:49:24 +0300

    Update report_wizard.py

--- addons/smarternows_accounts_app/models/report_wizard.py
+++ addons/smarternows_accounts_app/models/report_wizard.py
@@ -2,7 +2,18 @@ from odoo import models, fields
 
 class ReportWizard(models.TransientModel):
     _name = 'report.wizard'
-    _description = 'Report Wizard'
+    _description = 'Generic Report Wizard'
 
-    date_from = fields.Date(string="Start Date")
-    date_to = fields.Date(string="End Date")
+    date_from = fields.Date(string="Start Date", required=True)
+    date_to = fields.Date(string="End Date", required=True)
+
+    def action_generate_report(self):
+        return {
+            'type': 'ir.actions.report',
+            'report_name': 'smarternows_accounts_app.cash_flow_template',
+            'report_type': 'qweb-pdf',
+            'context': {
+                'date_from': self.date_from,
+                'date_to': self.date_to,
+            }
+        }
