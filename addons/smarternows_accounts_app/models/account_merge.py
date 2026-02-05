e26a8d9ca133d949d72b31271a54f409b4081762
JONATHAN KIRANGA <kirangajon@gmail.com>
2026-02-05 12:36:39 +0300

    Update account_merge.py

--- addons/smarternows_accounts_app/models/account_merge.py
+++ addons/smarternows_accounts_app/models/account_merge.py
@@ -2,15 +2,18 @@ from odoo import models, fields
 
 class AccountMerge(models.Model):
     _name = 'account.merge'
-    _description = 'Unified Accounting View'
+    _description = 'Unified Accounting Transactions'
 
-    name = fields.Char(string="Transaction Name")
+    name = fields.Char(string="Transaction Reference")
+    date = fields.Date(string="Date")
+    partner_id = fields.Many2one('res.partner', string="Partner")
     transaction_type = fields.Selection([
         ('sale', 'Sale'),
         ('purchase', 'Purchase'),
         ('bank', 'Bank'),
         ('journal', 'Journal'),
         ('asset', 'Asset'),
-    ], string="Type")
+    ], string="Transaction Type")
     amount = fields.Float(string="Amount")
-    date = fields.Date(string="Date")
+    currency_id = fields.Many2one('res.currency', string="Currency")
+
